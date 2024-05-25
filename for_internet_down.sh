6584433c5d3fc07df776ba8b74bd26563f9ff80f
Sreyashi Chowdhury <chowdhurysreyashi123@gmail.com>
2024-05-25 01:57:15 -0400

    Add files via upload

---
+++ for_internet_down.sh
@@ -0,0 +1,23 @@
+#!/bin?bash
+
+#is my internet down?
+
+echo "What so you want to check?"
+
+read target
+
+while true
+
+do
+
+if ping -q -c 2 -w 1 $target > /dev/null ; then
+	echo "Hey,you're up!!"
+	break
+else
+	echo "$target is currently down."
+fi
+
+sleep 2
+done
+
+
